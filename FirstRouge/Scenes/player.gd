eda352f61d2ca7f45e6716544533a8aa1f711ae2
SantoTinio <edillorluke@gmail.com>
2023-12-08 21:19:19 +0800

    Update player.gd

--- FirstRouge/Scenes/player.gd
+++ FirstRouge/Scenes/player.gd
@@ -7,7 +7,6 @@ var screen_size # Game window
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
