b3224a97d7c3f4b301126c5d4298f4b966fef201
SantoTinio <edillorluke@gmail.com>
2023-12-10 21:07:30 +0800

    Update HUD.gd

--- FirstRouge/Scripts/HUD.gd
+++ FirstRouge/Scripts/HUD.gd
@@ -3,6 +3,25 @@ extends CanvasLayer
 signal start_game
 
 # Called when the node enters the scene tree for the first time.
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+	
+func show_game_over():
+	show_message("Game Over")
+	await $MessageTimer.timeout
+	
+	$Message.text = "Dodge the Creeps!"
+	$Message.show()
+	
+	#Making a one-shot timer and wait for it to fin.
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+	
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
 func _ready():
 	pass # Replace with function body.
 
