6431fe9e00530a4d3a62e527e6e845ce33b85cb5
SantoTinio <edillorluke@gmail.com>
2023-12-10 21:50:03 +0800

    Updates
    Working game, not polished

--- FirstRouge/Scripts/main.gd
+++ FirstRouge/Scripts/main.gd
@@ -1,11 +1,11 @@
 extends Node
 
 @export var mob_scene: PackedScene
-var score
+var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	$Player.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,11 +16,16 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
+	$Player.show()
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mob", "queue_free")
 
 
 func _on_mob_timer_timeout():
@@ -51,6 +56,8 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
+	
 
 
 func _on_start_timer_timeout():
